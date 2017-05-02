e20effaeaa11610e8a09cbca9df8fc4ea9b540a4
DavidMBP <steadypathapp@gmail.com>
2017-05-01 21:08:56 -0500

    Finished the carousel section

---
+++ frontend/js/components/Carousel.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Slider from 'react-slick'
+
+const imgs = [
+  {src: 'http://placehold.it/1200x400', to: '#'},
+  {src: 'http://placehold.it/1200x400', to: '#'},
+  {src: 'http://placehold.it/1200x400', to: '#'},
+  {src: 'http://placehold.it/1200x400', to: '#'},
+  {src: 'http://placehold.it/1200x400', to: '#'}
+]
+
+export default class Carousel extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+
+    const carList = imgs.map( (img, index) =>
+      <Link to={img.to} key={index}>
+        <img className="carousel__img" src={img.src} alt=""/>
+      </Link>
+    )
+
+    var settings = {
+      dots: true,
+      infinite: true,
+      slidesToScroll: 1,
+      draggable: false,
+      autoplay: true,
+      nextArrow: <NextArrow/>,
+      prevArrow: <PreviousArrow/>,
+    }
+
+    return (
+      <div className="carousel">
+        <Slider {...settings}>
+          {carList}
+        </Slider>
+      </div>
+    )
+  }
+}
+
+const NextArrow = (props) => {
+  return (
+    <button onClick={props.onClick} className={props.className}>
+      <i className="material-icons">keyboard_arrow_right</i>
+    </button>
+  )
+}
+
+const PreviousArrow = (props) => {
+  return (
+    <button onClick={props.onClick} className={props.className}>
+      <i className="material-icons">keyboard_arrow_left</i>
+    </button>
+  )
+}
