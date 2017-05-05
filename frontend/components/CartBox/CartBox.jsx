a68752aa930b8c4792eb2b775701d922abe74487
DavidMBP <steadypathapp@gmail.com>
2017-05-05 12:21:36 -0500

    Created the cart box

---
+++ frontend/components/CartBox/CartBox.jsx
@@ -0,0 +1,48 @@
+import React, { PropTypes } from 'react'
+import { Link } from 'react-router-dom'
+
+
+import {order} from '../../../fake'
+const ord = order();
+
+export default class Cart extends React.Component {
+
+  render () {
+    const detList = ord.details.map( (detail, index) =>
+      <CartItem key={index} {...detail}/>
+    )
+
+    return (
+      <div className="cart__box smooth">
+        <h3>Tu Carrito</h3>
+
+        <div className="cart__mid">
+          {ord.details.length == 0 && <h4>No Hay Productos</h4>}
+          <ul>
+            {detList}
+          </ul>
+        </div>
+
+        {ord.details.length > 0 &&
+          <div className="cart__bottom smooth">
+            <p><span>Sin IVA</span> Subtotal: ${ord.subtotal}</p>
+
+            <Link className="button light" to="#">Detalles</Link>
+          </div>
+        }
+      </div>
+    )
+  }
+}
+
+
+const CartItem = props => (
+  <li className="cart__item">
+    <img src={props.pimg}/>
+    <div>
+      <p className="overflow-text">{props.description}</p>
+      <p><span>Cantidad:</span> {props.quantity}</p>
+      <p><span>Precio:</span> ${props.price} </p>
+    </div>
+  </li>
+)
