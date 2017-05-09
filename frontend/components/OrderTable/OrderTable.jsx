4690149634894e6668263bd1e9b999a457000e3c
DavidMBP <steadypathapp@gmail.com>
2017-05-08 21:19:46 -0500

    Added the option to make the ordertable not editable

--- frontend/components/OrderTable/OrderTable.jsx
+++ frontend/components/OrderTable/OrderTable.jsx
@@ -1,4 +1,5 @@
 import React, { PropTypes } from 'react'
+import classnames from 'classnames'
 
 import {order} from '../../../fake.js'
 const ords = order()
@@ -15,22 +16,27 @@ class OrderTable extends React.Component {
 
   render () {
 
+    const colSpan = this.props.editable ? 5 : 4
+    const tableClasses = classnames('order__table', this.props.size)
+
     const detList = ords.details.map( (detail, index) =>
       <OrderRow
         key={index} {...detail}
+        editable={this.props.editable}
         onRequestChange={this.handleInputChange.bind(this, index)}/>
     )
 
     return (
-      <table className="order__table">
+      <table className={tableClasses}>
         <thead>
           <tr>
             <th>Producto</th>
             <th>Descripcion</th>
             <th>Precio</th>
             <th>Cantidad</th>
-            <th>Subtotal <span>Sin IVA</span></th>
-            <th></th>
+            <th className="overflow-text">Subtotal <span>Sin IVA</span></th>
+            {this.props.editable && <th></th>}
+
           </tr>
         </thead>
 
@@ -40,23 +46,23 @@ class OrderTable extends React.Component {
 
         <tfoot className="order__table-foot">
           <tr>
-            <td colSpan="5">Subtotal: </td>
+            <td colSpan={colSpan}>Subtotal: </td>
             <td className="foot-main">${ords.subtotal}</td>
           </tr>
           <tr>
-            <td colSpan="5">IVA:</td>
+            <td colSpan={colSpan}>IVA:</td>
             <td className="foot-main">${ords.iva}</td>
           </tr>
           <tr>
-            <td colSpan="5">Discount:</td>
+            <td colSpan={colSpan}>Discount:</td>
             <td className="foot-main">${ords.discount}</td>
           </tr>
           <tr>
-            <td colSpan="5">Costo de Envío: </td>
+            <td colSpan={colSpan}>Costo de Envío: </td>
             <td className="foot-main">${ords.shippingCost}</td>
           </tr>
           <tr>
-            <td colSpan="5">Total:</td>
+            <td colSpan={colSpan}>Total:</td>
             <td className="foot-main">${ords.total}</td>
           </tr>
 
@@ -85,15 +91,22 @@ const OrderRow = (props) => (
     </td>
 
     <td>
-      <input className="input input-quantity" type="number" max="10" min="0" value={props.quantity} onChange={props.onRequestChange}/>
+      {props.editable ?
+        <input className="input input-quantity" type="number" max="10" min="0" value={props.quantity} onChange={props.onRequestChange}/>
+        :
+        props.quantity
+      }
     </td>
 
     <td>
       ${props.subtotal}
     </td>
 
-    <td>
-      <a href="#">Eliminar</a>
-    </td>
+    {props.editable &&
+      <td>
+        <a href="#">Eliminar</a>
+      </td>
+    }
+
   </tr>
 )
