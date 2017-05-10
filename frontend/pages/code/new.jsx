542466f07be5ccfdc182da559486c0781937b457
DavidMBP <steadypathapp@gmail.com>
2017-05-09 22:06:49 -0500

    Created discout code new

---
+++ frontend/pages/code/new.jsx
@@ -0,0 +1,13 @@
+import React, { PropTypes } from 'react'
+import DiscountForm from 'components/DiscountForm/DiscountForm'
+
+class CodesNew extends React.Component {
+  render () {
+
+    return (
+      <DiscountForm/>
+    )
+  }
+}
+
+export default CodesNew;
