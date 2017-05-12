d5dab3e9eadc97ad6b1b973e2c82459a2d118493
DavidMBP <steadypathapp@gmail.com>
2017-05-11 21:16:28 -0500

    Added the webpack production config

---
+++ webpack.config.production.js
@@ -0,0 +1,59 @@
+var webpack = require('webpack');
+var path = require('path');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+
+
+var config = {
+  entry: './frontend/index.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  resolve: {
+    modules: [
+      'node_modules',
+      'frontend'
+    ],
+    extensions: ['.js', '.jsx', '.scss', '.sass']
+  },
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      exclude: /(node_modules)/,
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: [['es2015', {modules: false}], 'react'],
+          plugins: ["transform-decorators-legacy"]
+        }
+      }]
+    },{
+      test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+      loader: 'file-loader?name=fonts/[name].[ext]'
+    },{
+      test: /\.(scss|sass|css)$/,
+      use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+    }]
+  },
+  plugins: [
+    new ExtractTextPlugin("styles.css"),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.optimize\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorOptions: { discardComments: {removeAll: true } },
+      canPrint: true
+    }),
+    new UglifyJSPlugin(),
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    new BundleAnalyzerPlugin()
+  ],
+}
+
+module.exports = config
