259acfcdc9c5cb654af0a232a630fa5872b3587c
DavidMBP <steadypathapp@gmail.com>
2017-05-13 15:19:00 -0500

    Added host 0000

--- webpack.config.js
+++ webpack.config.js
@@ -40,9 +40,10 @@ var config = {
     }]
   },
   devServer: {
-    host: 'localhost',
+    host: '0.0.0.0',
     port: 3000,
     historyApiFallback: true,
+    disableHostCheck: true,
     hot: true,
   },
   plugins: [
