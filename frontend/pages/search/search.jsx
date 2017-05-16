bea3e0e4cb570bbd24af9612791dcc9ba6524ce9
DavidMBP <steadypathapp@gmail.com>
2017-05-15 22:34:43 -0500

    Added products action

--- frontend/pages/search/search.jsx
+++ frontend/pages/search/search.jsx
@@ -1,23 +1,68 @@
 import React, { PropTypes } from 'react'
 import Results from 'components/Results/Results'
+import { connect } from 'react-redux'
+import { getProducts, resetProducts } from 'actions/products'
+import queryString from 'query-string'
 
 /**
 * HTTP - GET
 * @param {array} products - results of the search input
 *
-* LOCAL - POST (on unmount)
-* @param {array} reset - An empty array to reset products array
 */
 
+@connect( store => {
+  return {
+    products: store.products
+  }
+})
 class Search extends React.Component {
-  render () {
+  constructor(props){
+    super(props)
 
+    this.handleUrlChanged = this.handleUrlChanged.bind(this)
+    this.handleGlobalError = this.handleGlobalError.bind(this)
+    this.unlisten = null
+  }
+
+  componentDidMount() {
+    this.handleUrlChanged(this.props.history.location, this.props.history.action)
+    this.unlisten = this.props.history.listen(this.handleUrlChanged)
+  }
+
+  componentWillUnmount() {
+    this.unlisten()
+    this.props.dispatch(resetProducts())
+  }
+
+  handleUrlChanged(location, action) {
+    if(this.props.match.url === location.pathname){
+      const parse = queryString.parse(location.search)
+      this.props.dispatch(getProducts(parse.q, undefined, parse.page, parse.sort))
+      .catch(this.handleGlobalError)
+    }
+  }
+
+  handleGlobalError(response) {
+
+  }
+
+  render () {
+    const {
+      products,
+      history,
+      match
+    } = this.props
+
+    const parse = queryString.parse(history.location.search)
     return (
       <main>
         <p className="search-label">Resultados para:
-          "<i><span className="sub-text light">search value</span></i>"
+          "<i><span className="sub-text light">{parse.q}</span></i>"
         </p>
-        <Results/>
+        <Results
+          products={products.results}
+          total={products.total}
+          url={`${match.url}?q=${parse.q}&`}/>
       </main>
     )
   }
