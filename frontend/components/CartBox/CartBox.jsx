2102b3b79429d3f6208ad998ebaab8c53771f6e0
DavidMBP <steadypathapp@gmail.com>
2017-05-15 22:31:38 -0500

    Changed how the cartbox is shown

--- frontend/components/CartBox/CartBox.jsx
+++ frontend/components/CartBox/CartBox.jsx
@@ -1,30 +1,47 @@
 import React, { PropTypes } from 'react'
 import { Link } from 'react-router-dom'
+import classnames from 'classnames'
 
-const ord = {details: []}
 
 export default class CartBox extends React.Component {
+  constructor(props){
+    super(props)
+
+    this.handleMouseLeave = this.handleMouseLeave.bind(this)
+  }
+
+  handleMouseLeave(event) {
+    document.body.style.overflow = ""
+    this.props.onRequestMouseLeave()
+  }
 
   render () {
-    const detList = ord.details.map( (detail, index) =>
+    const {
+      details,
+      show
+    } = this.props
+
+    const detList = details.map( (detail, index) =>
       <CartItem key={index} {...detail}/>
     )
 
+    const boxClasses = classnames('cart-box', {'show': show})
+
     return (
       <div
-        className="cart-box"
+        className={boxClasses}
         onMouseEnter={(event) => document.body.style.overflow = "hidden"}
-        onMouseLeave={(event) => document.body.style.overflow = ""}>
+        onMouseLeave={this.handleMouseLeave}>
         <h3>Tu Carrito</h3>
 
         <div className="cart-mid">
-          {ord.details.length == 0 && <h4>No Hay Productos</h4>}
+          {details.length == 0 && <h4 className="sub-text">No Hay Productos</h4>}
           <ul>
             {detList}
           </ul>
         </div>
 
-        {ord.details.length > 0 &&
+        {details.length > 0 &&
           <div className="cart-bottom">
             <Link className="light-button margin-button" to="/cart">Detalles</Link>
             <Link className="dark-button margin-button" to="/checkout">Checkout</Link>
@@ -40,7 +57,7 @@ const CartItem = props => (
   <li className="cart-item">
     <img src={props.pimg}/>
     <div>
-      <p className="overflow-text">{props.description}</p>
+      <p className="overflow-text">{props.name}</p>
       <p><span className="sub-text">Cantidad:</span> {props.quantity}</p>
       <p><span className="sub-text">Precio:</span> ${props.price} </p>
     </div>
