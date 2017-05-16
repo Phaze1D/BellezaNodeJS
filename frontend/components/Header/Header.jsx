0522057590e49ed1ed5c834513571a54f0251bca
DavidMBP <steadypathapp@gmail.com>
2017-05-15 22:32:43 -0500

    Added redux action for categories and cart

--- frontend/components/Header/Header.jsx
+++ frontend/components/Header/Header.jsx
@@ -1,26 +1,45 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, Redirect } from 'react-router-dom'
 import CartBox from 'components/CartBox/CartBox'
 import classnames from 'classnames'
+import { connect } from 'react-redux'
+import { getCategories } from 'actions/categories'
+import { hideCart } from 'actions/cart'
 
-const cats = []
 
 /**
-* HTTP - GET
-* @param {object} cartOrder - The Object the represent the current order (shopping cart)
+* LOCAL - GET
+* @param {object} cart - The Object the represent the current order (shopping cart)
 *
 * HTTP - GET
 * @param {array} categories - An array of all the categories
-
-* LOCAL - POST
-* @param {string} searchInput - Sets the search input
 */
 
+@connect( store => {
+  return {
+    categories: store.categories,
+    cart: store.cart
+  }
+})
 export default class Header extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {showCart: false}
+
     this.handleToggleSide = this.handleToggleSide.bind(this)
     this.handleNavClick = this.handleNavClick.bind(this)
+    this.handleSearchSubmit = this.handleSearchSubmit.bind(this)
+    this.handleGlobalError = this.handleGlobalError.bind(this)
+  }
+
+  componentDidMount() {
+    this.props.dispatch(getCategories()).catch(this.handleGlobalError)
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    if(nextProps.cart.show && !this.props.cart.show) {
+      setTimeout( () => {this.dispatch(hideCart())}, 4000)
+    }
   }
 
   handleNavClick(event) {
@@ -42,21 +61,47 @@ export default class Header extends React.Component {
     this.refs.side.classList.toggle('show')
   }
 
+  handleSearchSubmit(event) {
+    event.preventDefault()
+    let input = event.target.elements['query']
+    this.props.history.push({
+      pathname: '/search',
+      search: `?q=${input.value}&page=0&sort=0`
+    })
+    input.value = ''
+  }
+
+  handleGlobalError(response) {
+
+  }
+
   render() {
-    const navList = cats.map( (category) =>
+    const {
+      categories,
+      cart
+    } = this.props
+
+    const showC = this.state.showCart || cart.show
+
+    const navList = categories.map( (category, index) =>
       <li
         key={category.id}
         className="nav-item"
         onClick={this.handleNavClick}
         onMouseLeave={this.handleNavLeave}>
 
-        <Link to="/categories" title={category.name} className="nav-link">{category.name}</Link>
-        <MainList mains={category.mains}/>
+        <Link to={category.to} title={category.name} className="nav-link">{category.name}</Link>
+        <MainList subs={category.subs}/>
       </li>
     )
 
     return (
       <header>
+        <CartBox
+          {...cart}
+          show={showC}
+          onRequestMouseLeave={(event) => {this.setState({showCart: false})} }/>
+
         <div className="grid center">
           <Link className="grid center overflow-text col-8 col-sm-11" to="/home" title="Neals Yard Remedies Mexico">
             <img id="logo-img" src="http://placehold.it/200" alt="logo"/>
@@ -77,17 +122,19 @@ export default class Header extends React.Component {
               <hr className="vertical-hr"></hr>
               <Link className="dark-a header-link" to="/user/details" title="Signin">Mi Cuenta</Link>
               <hr className="vertical-hr"></hr>
-              <div id="cart-link" className="header-link">
+              <div
+                id="cart-link"
+                className="header-link"
+                onMouseEnter={(event) => {this.setState({showCart: true})} }>
                 <Link className="dark-a grid center" to="/cart">
                   Carrito
                   <i className="material-icons">shopping_cart</i>
                 </Link>
-                <CartBox/>
               </div>
             </div>
 
-            <form className="search-form grid center">
-              <input type="search" name="search"/>
+            <form className="search-form grid center" onSubmit={this.handleSearchSubmit}>
+              <input type="search" name="query" autoComplete="off"/>
               <button type="submit">
                 <i className="material-icons">search</i>
               </button>
@@ -119,8 +166,8 @@ export default class Header extends React.Component {
             </li>
 
             <li className="nav-item col-sm-show col-hide">
-              <form className="search-form grid center">
-                <input type="search" name="search"/>
+              <form className="search-form grid center" onSubmit={this.handleSearchSubmit}>
+                <input type="search" name="query" autoComplete="off"/>
                 <button type="submit">
                   <i className="material-icons">search</i>
                 </button>
@@ -138,11 +185,11 @@ export default class Header extends React.Component {
 
 const MainList = (props) => {
 
-  const mainList = props.mains.map( (main) => {
+  const mainList = props.subs.map( (main) => {
 
     const subList = main.subs.map( (sub) =>
       <li key={sub.id} title={sub.name}>
-        <Link to="/category" className="sub-text">
+        <Link to={sub.to} className="sub-text">
           {sub.name}
         </Link>
       </li>
@@ -150,7 +197,7 @@ const MainList = (props) => {
 
     return (
       <ul key={main.id} className="main-item">
-        <Link to="#" title={main.name} className="dark-a">{main.name}</Link>
+        <Link to={main.to} title={main.name} className="dark-a">{main.name}</Link>
         {subList}
       </ul>
     )
