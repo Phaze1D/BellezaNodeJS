c9613498fe068bc03f97c9c450551c0ed2fcac5c
DavidMBP <steadypathapp@gmail.com>
2017-05-15 22:36:06 -0500

    Added redux

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from "react-redux"
+import { applyMiddleware, createStore } from "redux"
+import promise from "redux-promise-middleware"
 import { Routes } from 'utils/routes'
+import reducers from 'reducers'
+
 
 import 'index.sass'
 
+let middleware = null
+
+if(process.env.NODE_ENV === 'production'){
+  middleware = applyMiddleware( promise({promiseTypeSuffixes: ['LOADING', 'SUCCESS', 'ERROR']}))
+}else {
+  var logger = require('redux-logger').createLogger
+  middleware = applyMiddleware(logger(), promise({promiseTypeSuffixes: ['LOADING', 'SUCCESS', 'ERROR']}))
+}
+
+const store = createStore(reducers, middleware)
+
 ReactDOM.render(
-  <Routes/>,
+  <Provider store={store}><Routes/></Provider>,
   document.getElementById('app')
 )
