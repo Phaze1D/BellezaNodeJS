3fa8b36d1a75ae9f4a5a10bd4176736f6af36396
DavidMBP <steadypathapp@gmail.com>
2017-05-16 22:36:17 -0500

    Added missing reducers

--- frontend/reducers/index.js
+++ frontend/reducers/index.js
@@ -1,18 +1,70 @@
-import { combineReducers } from "redux"
+import { combineReducers } from "redux-immutable"
 import reduceReducers from "reduce-reducers"
+import Immutable from 'immutable'
+
 
 import {
   getCategoriesReducer,
-} from 'reducers/categories'
+} from 'reducers/category'
 
 import {
   getProductsReducer,
   resetProductsReducer,
   getProductReducer,
   resetProductReducer
-} from 'reducers/products'
+} from 'reducers/product'
 
-import { addDetailReducer } from 'reducers/cart'
+import {
+  addDetailReducer,
+  hideCartReducer,
+  changeQuantityReducer,
+  removeDetailReducer
+ } from 'reducers/cart'
+
+import {
+  userSignUpReducer,
+  userLoginReducer,
+  validateUserNewReducer,
+  userUpdateReducer,
+  validateUserUpdateReducer,
+  userPreferenceReducer,
+  resetUserErrorsReducer,
+  userLogoutReducer
+} from 'reducers/user'
+
+import {
+  contactReducer,
+  validateContactReducer,
+  resetOthersErrorsReducer
+} from 'reducers/others'
+
+import {
+  addressNewReducer,
+  addressUpdateReducer,
+  validateAddressReducer,
+  addressDeleteReducer
+} from 'reducers/address'
+
+const othersReducer = reduceReducers(
+  contactReducer,
+  validateContactReducer,
+  resetOthersErrorsReducer
+)
+
+const userReducer = reduceReducers(
+  userSignUpReducer,
+  userLoginReducer,
+  validateUserNewReducer,
+  userUpdateReducer,
+  validateUserUpdateReducer,
+  userPreferenceReducer,
+  resetUserErrorsReducer,
+  userLogoutReducer,
+  addressNewReducer,
+  addressUpdateReducer,
+  validateAddressReducer,
+  addressDeleteReducer
+)
 
 const categoriesReducer = reduceReducers(
   getCategoriesReducer,
@@ -30,11 +82,25 @@ const productReducer = reduceReducers(
 
 const cartReducer = reduceReducers(
   addDetailReducer,
+  hideCartReducer,
+  changeQuantityReducer,
+  removeDetailReducer
 )
+
+const StateRecord = Immutable.Record({
+  categories: undefined,
+  cart: undefined,
+  products: undefined,
+  product: undefined,
+  user: undefined,
+  others: undefined
+});
 
 export default combineReducers({
   categories: categoriesReducer,
   cart: cartReducer,
   products: productsReducer,
-  product: productReducer
-})
+  product: productReducer,
+  user: userReducer,
+  others: othersReducer
+}, StateRecord)
