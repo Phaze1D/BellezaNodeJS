26a247057ba850b666276314b7bb4e789564b9ce
DavidMBP <steadypathapp@gmail.com>
2017-05-16 22:33:36 -0500

    Added others action to contact and reset

--- frontend/pages/others/contact.jsx
+++ frontend/pages/others/contact.jsx
@@ -1,23 +1,86 @@
 import React, { PropTypes } from 'react'
+import { connect } from 'react-redux'
+import {
+  contact,
+  validateContact,
+  resetOthersErrors
+} from 'actions/others'
 
-/**
-* HTTP - POST
-* @param {object} question - The question object to ask
-*/
+
+
+@connect( store => {
+  return {
+    others: store.others
+  }
+})
 class Contact extends React.Component {
+  constructor(props){
+    super(props)
+
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleInputBlur = this.handleInputBlur.bind(this)
+    this.handleInputFocus = this.handleInputFocus.bind(this)
+    this.handleError = this.handleError.bind(this)
+  }
+
+  handleSubmit(event){
+    event.preventDefault()
+    let elements = event.target.elements
+    let formData = new FormData()
+    formData.append('email', elements.email)
+    formData.append('name', elements.name)
+    formData.append('question', elements.question)
+
+    this.props.dispatch(contact(formData))
+    .then()
+    .catch(this.handleError)
+  }
+
+  handleInputBlur(event){
+    let fieldData = new FormData()
+    fieldData.append(event.target.name, event.target.value)
+
+    this.props.dispatch(validateContact(fieldData))
+    .then()
+    .catch(this.handleError)
+  }
+
+  handleInputFocus(event){
+    this.props.dispatch(resetOthersErrors(event.target.name))
+  }
+
+  handleError(response){
+
+  }
+
   render () {
+    const errors = this.props.others.get('errors')
 
     return (
       <main>
         <h2>Contáctanos</h2>
-        <form className="main-form" style={{maxWidth: '400px', width: '100%'}}>
+        <form
+          className="main-form"
+          style={{maxWidth: '400px', width: '100%'}}
+          onSubmit={this.handleSubmit}>
+
           <label htmlFor="name">Nombre</label>
-          <input type="text" name="name"/>
+          {errors.get('name') && <div className="error-div">{errors.get('name')}</div>}
+          <input type="text" name="name"
+            onBlur={this.handleInputBlur}
+            onFocus={this.handleInputFocus}/>
+
           <label htmlFor="email">Email</label>
-          <input type="text" name="email"/>
+          {errors.get('email') && <div className="error-div">{errors.get('email')}</div>}
+          <input type="text" name="email"
+            onBlur={this.handleInputBlur}
+            onFocus={this.handleInputFocus}/>
 
           <label htmlFor="question">Pregunta</label>
-          <textarea name="question" className="input" rows="5" cols="40"></textarea>
+          {errors.get('question') && <div className="error-div">{errors.get('question')}</div>}
+          <textarea name="question" className="input" rows="5" cols="40"
+            onBlur={this.handleInputBlur}
+            onFocus={this.handleInputFocus}></textarea>
 
           <input className="submit full" type="submit" value="Enviar"/>
         </form>
--- frontend/pages/others/reset.jsx
+++ frontend/pages/others/reset.jsx
@@ -1,13 +1,43 @@
 import React, { PropTypes } from 'react'
+import { connect } from 'react-redux'
+import { passwordReset } from 'actions/others'
 
 /**
 * HTTP - POST
 * @param {object} passwordReset - The password reset info
 */
 
+@connect( store => {
+  return {
+    others: store.others
+  }
+})
 class PasswordReset extends React.Component {
-  render () {
+  constructor(props){
+    super(props)
 
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleError = this.handleError.bind(this)
+  }
+
+  handleSubmit(event){
+    event.preventDefault()
+    let elements = event.target.elements
+    let formData = new FormData()
+    formData.append('email', elements.email)
+
+    this.props.dispatch(passwordReset(formData))
+    .then()
+    .catch(this.handleError)
+  }
+
+  handleError(response){
+
+  }
+
+  render () {
+    const errors = this.props.others.get('errors')
+    
     return (
       <main>
         <h2>¿Olvidaste Tu Contraseña?</h2>
@@ -15,8 +45,9 @@ class PasswordReset extends React.Component {
 					Proporcione a su cuenta de correo electrónico para recibir un correo electrónico para restablecer su contraseña
 				</p>
 
-        <form className="main-form" style={{width: '100%', maxWidth: '400px'}}>
+        <form className="main-form" style={{width: '100%', maxWidth: '400px'}} onSubmit={this.handleSubmit}>
           <label htmlFor="email">Email</label>
+          {errors.get('email') && <div className="error-div">{errors.get('email')}</div>}
           <input name="email" type="text"/>
           <input  className="submit full"  type="submit" value="Enviar"/>
         </form>
