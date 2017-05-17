2f59c1319af3ebaffd9ab1686179d970a539016a
DavidMBP <steadypathapp@gmail.com>
2017-05-16 22:21:43 -0500

    Rename file

---
+++ frontend/actions/category.js
@@ -0,0 +1,10 @@
+import * as types from 'actions/types'
+import axios from "axios"
+
+
+export const getCategories = () => {
+  return {
+    type: types.GET_CATEGORIES,
+    payload: axios.get('/api/categories')
+  }
+}
