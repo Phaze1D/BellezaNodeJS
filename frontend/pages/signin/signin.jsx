bf824099fcb98a8c0702e01883e05ead2f3b5e8d
DavidMBP <steadypathapp@gmail.com>
2017-05-16 22:33:52 -0500

    Add user action sign forms

--- frontend/pages/signin/signin.jsx
+++ frontend/pages/signin/signin.jsx
@@ -1,5 +1,12 @@
 import React, { PropTypes } from 'react'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import {
+  userSignUp,
+  userLogin,
+  validateUserNew,
+  resetUserErrors
+} from 'actions/user'
 
 /**
 * HTTP - POST
@@ -18,16 +25,84 @@ import { Link } from 'react-router-dom'
 * @param {object} resetAll - An empty object to reset errors
 */
 
+@connect( store => {
+  return {
+    user: store.user
+  }
+})
 export default class Signin extends React.Component {
+  constructor(props){
+    super(props)
+
+    this.handleLogin = this.handleLogin.bind(this)
+    this.handleSignUp = this.handleSignUp.bind(this)
+    this.handleInputBlur = this.handleInputBlur.bind(this)
+    this.handleInputFocus = this.handleInputFocus.bind(this)
+    this.handleError = this.handleError.bind(this)
+  }
+
+  handleLogin(event){
+    event.preventDefault()
+    let elements = event.target.elements
+    let formData = new FormData()
+    formData.append('email', elements.email)
+    formData.append('password', elements.password)
+
+    this.props.dispatch(userLogin(formData))
+    .then()
+    .catch(this.handleError)
+  }
+
+  handleSignUp(event){
+    event.preventDefault()
+    let elements = event.target.elements
+    let formData = new FormData()
+    formData.append('email', elements.email)
+    formData.append('password', elements.password)
+    formData.append('firstName', elements.firstName)
+    formData.append('lastName', elements.lastName)
+
+    this.props.dispatch(userSignUp(formData))
+    .then()
+    .catch(this.handleError)
+  }
+
+  handleInputBlur(event){
+    let fieldData = new FormData()
+    fieldData.append(event.target.name, event.target.value)
+
+    this.props.dispatch(validateUserNew(fieldData))
+    .then()
+    .catch(this.handleError)
+  }
+
+  handleInputFocus(event){
+    this.props.dispatch(resetUserErrors(event.target.name))
+  }
+
+  handleError(response){
+
+  }
+
   render () {
+    const {
+      user
+    } = this.props
 
     return (
       <main>
         <h3>Iniciar Sesión o Crea Nuevo Usuario</h3>
 
         <div className="grid-wrap around">
-          <LoginFrom errors={{}}/>
-          <SignupForm errors={{}}/>
+          <LoginFrom
+            errors={user.get('errors')}
+            onRequestSubmit={this.handleLogin}/>
+
+          <SignUpForm
+            errors={user.get('errors')}
+            onRequestSubmit={this.handleSignUp}
+            onRequestBlur={this.handleInputBlur}
+            onRequestFocus={this.handleInputFocus}/>
         </div>
       </main>
     )
@@ -40,13 +115,12 @@ const LoginFrom = props => {
     <section className="col-5 col-xs-10">
       <h4 className="h-underline">Iniciar Sesión</h4>
       <h5 className="sub-text">Si usted ya tiene una cuenta, ingrese aquí.</h5>
-      <form className="main-form">
+      <form className="main-form" onSubmit={props.onRequestSubmit}>
         <label htmlFor="email">Email</label>
-        {props.errors.last_name && <div className="error-div">{props.errors.email}</div>}
-        <input type="text" name="email"/>
+        {props.errors.get('login') && <div className="error-div">{props.errors.get('login')}</div>}
+        <input type="text" name="email" />
 
         <label htmlFor="password">Contraseña <Link to="/reset" className="sub-text light" style={{float: 'right'}}>(Olvidó su Contraseña)</Link></label>
-        {props.errors.last_name && <div className="error-div">{props.errors.password}</div>}
         <input type="password" name="password"/>
 
         <input className="submit full" type="submit" value="Ingresar"/>
@@ -55,7 +129,7 @@ const LoginFrom = props => {
   )
 }
 
-const SignupForm = props => {
+const SignUpForm = props => {
 
   return (
     <section className="col-5 col-xs-10">
@@ -64,22 +138,30 @@ const SignupForm = props => {
 				Al crear una cuenta en nuestra tienda, usted será capaz de moverse a través del proceso
         de pago más rápido, guardar múltiples direcciones de envío.
       </h5>
-      <form className="main-form">
-        <label htmlFor="first_name">Nombre</label>
-        {props.errors.first_name && <div className="error-div">{props.errors.first_name}</div>}
-        <input type="text" name="first_name"/>
+      <form className="main-form" onSubmit={props.onRequestSubmit}>
+        <label htmlFor="firstName">Nombre</label>
+        {props.errors.get('firstName') && <div className="error-div">{props.errors.get('firstName')}</div>}
+        <input type="text" name="firstName"
+          onBlur={props.onRequestBlur}
+          onFocus={props.onRequestFocus}/>
 
-        <label htmlFor="last_name">Apellido</label>
-        {props.errors.last_name && <div className="error-div">{props.errors.last_name}</div>}
-        <input type="text" name="last_name"/>
+        <label htmlFor="lastName">Apellido</label>
+        {props.errors.get('lastName') && <div className="error-div">{props.errors.get('lastName')}</div>}
+        <input type="text" name="lastName"
+          onBlur={props.onRequestBlur}
+          onFocus={props.onRequestFocus}/>
 
         <label htmlFor="email">Email</label>
-        {props.errors.email && <div className="error-div">{props.errors.email}</div>}
-        <input type="text" name="email"/>
+        {props.errors.get('email') && <div className="error-div">{props.errors.get('email')}</div>}
+        <input type="text" name="email"
+          onBlur={props.onRequestBlur}
+          onFocus={props.onRequestFocus}/>
 
         <label htmlFor="password">Contraseña </label>
-        {props.errors.password && <div className="error-div">{props.errors.password}</div>}
-        <input type="password" name="password"/>
+        {props.errors.get('password') && <div className="error-div">{props.errors.get('password')}</div>}
+        <input type="password" name="password"
+          onBlur={props.onRequestBlur}
+          onFocus={props.onRequestFocus}/>
 
         <input className="submit full" type="submit" value="Crear"/>
       </form>
