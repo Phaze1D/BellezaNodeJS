7e9e6c75d6e75cc6e2435e078300d8248b2ca6fc
DavidMBP <steadypathapp@gmail.com>
2017-05-17 21:58:15 -0500

    Connect to redux store

--- frontend/pages/order/show.jsx
+++ frontend/pages/order/show.jsx
@@ -2,8 +2,10 @@ import React, { PropTypes } from 'react'
 import OrderTable from 'components/OrderTable/OrderTable'
 import { Address } from 'pages/checkout/confirmation'
 import dateOptions from 'utils/date'
+import { connect } from 'react-redux'
+import { getOrder, resetOrder } from 'actions/order'
 
-const ord = {details: []}
+
 
 /**
 * HTTP - GET
@@ -13,25 +15,56 @@ const ord = {details: []}
 * @param {object} reset - reset the order with empty object
 */
 
+@connect( store => {
+  return {
+    order: store.order
+  }
+})
 class OrderShow extends React.Component {
+  constructor(props){
+    super(props)
+
+    this.handleError = this.handleError.bind(this)
+  }
+
+  componentDidMount() {
+    let id = this.props.match.params.id
+    this.props.dispatch(getOrder(id))
+    .then()
+    .catch(this.handleError)
+  }
+
+  componentWillUnmount() {
+    this.props.dispatch(resetOrder())
+  }
+
+  handleError(response) {
+
+  }
+
   render () {
+    const {
+      order
+    } = this.props
 
     return (
       <main>
-        <h2 style={{color: '#1eab30'}}>Status: Pago</h2>
-        <h4 className="sub-text">Referencia: #{ord.id}</h4>
+        <h2 className={`status-${order.get('status')}`}>Status: {order.get('status')}</h2>
+        <h4 className="sub-text">Referencia: #{order.get('id')}</h4>
         <h4 className="sub-text">
-          Fecha Realizado: {ord.date.toLocaleString('en-us', dateOptions)}
+          Fecha Realizado: {order.get('date').toLocaleString('en-us', dateOptions)}
         </h4>
         <div className="grid-wrap">
           <div className="col-9 col-md-8 col-sm-12">
-            <OrderTable editable={false}/>
+            <OrderTable
+              order={order}
+              editable={false}/>
           </div>
 
 
           <div className="col-3 col-md-4 col-sm-12">
-            <Address {...ord.shippedTo} title="Dirección de Envío"/>
-            <Address {...ord.invoiceTo} title="Facturacion"/>
+            <Address address={order.get('shippingAddress')} title="Dirección de Envío"/>
+            <Address address={order.get('invoiceAddress')} title="Facturacion"/>
           </div>
         </div>
       </main>
