11dc5be4a0fe721b9b7118d833271bbb7c29fec5
DavidMBP <steadypathapp@gmail.com>
2017-05-17 21:55:20 -0500

    Handles immutable type

--- frontend/components/OrderItem/OrderItem.jsx
+++ frontend/components/OrderItem/OrderItem.jsx
@@ -2,58 +2,71 @@ import React, { PropTypes } from 'react'
 import { Link } from 'react-router-dom'
 import dateOptions from 'utils/date'
 
-const OrderItem = (props) => {
-  const detailList = props.details.map( (detail, index) =>
-    <Detail key={index} {...detail} />
-  )
+class OrderItem extends React.PureComponent {
+  render() {
+    const {
+      order
+    } = this.props
 
-  return (
-    <article className="box">
-      <div className="grid order-item-top">
-        <p className="overflow-text col-xxs-hide">
-          Fecha Realizado
-          <span>{props.date.toLocaleString('en-us', dateOptions)}</span>
-        </p>
+    const detailList = order.get('details').map( (detail, index) =>
+      <Detail key={index} detail={detail} />
+    )
 
-        <p className="overflow-text col-xxs-hide">
-          Total
-          <span>${props.total}</span>
-        </p>
+    return (
+      <article className="box">
+        <div className="grid order-item-top">
+          <p className="overflow-text col-xxs-hide">
+            Fecha Realizado
+            <span>{order.get('date').toLocaleString('en-us', dateOptions)}</span>
+          </p>
 
-        <p className="overflow-text">
-          Status
-          <span style={{color: '#1eab30'}}>Enviado</span>
-        </p>
+          <p className="overflow-text col-xxs-hide">
+            Total
+            <span>${order.get('total')}</span>
+          </p>
 
-        <p className="overflow-text">
-          Referencia #{props.id}
-          <span>
-            <Link to="/order">Detalles</Link>
-          </span>
-        </p>
-      </div>
+          <p className="overflow-text">
+            Status
+            <span style={{color: '#1eab30'}}>order.get('status')</span>
+          </p>
 
-      {detailList}
-    </article>
-  )
+          <p className="overflow-text">
+            Referencia #{order.get('id')}
+            <span>
+              <Link to="/order">Detalles</Link>
+            </span>
+          </p>
+        </div>
+
+        {detailList}
+      </article>
+    )
+  }
 }
 
+class Detail extends React.PureComponent {
+  render() {
+    const {
+      detail
+    } = this.props
 
-const Detail = props => (
-  <div className="grid-wrap center">
-    <img src={props.pimg} className="col-2 col-xxs-3"/>
-    <div className="grow col-xxs-9">
-      <p style={{fontWeight: 'bold', color: 'black'}} className="overflow-text">{props.description}</p>
-      <p className="sub-text overflow-text">Precio: ${props.price} Cantidad: {props.quantity}</p>
-      <p className="sub-text primary">${props.subtotal}</p>
-    </div>
+    return (
+      <div className="grid-wrap center">
+        <img src={detail.get('pimg')} className="col-2 col-xxs-3"/>
+        <div className="grow col-xxs-9">
+          <p style={{fontWeight: 'bold', color: 'black'}} className="overflow-text">{detail.get('name')}</p>
+          <p className="sub-text overflow-text">Precio: ${detail.get('price')} Cantidad: {detail.get('quantity')}</p>
+          <p className="sub-text primary">${detail.get('subTotal')}</p>
+        </div>
 
-    <Link to="/product" className="secondary-button raise col-xxs-hide">
-      <span className="col-sm-hide">Comparar de Nuevo</span>
-      <span className="col-hide col-sm-show">Comparar</span>
-    </Link>
-  </div>
-)
+        <Link to="/product" className="secondary-button raise col-xxs-hide">
+          <span className="col-sm-hide">Comparar de Nuevo</span>
+          <span className="col-hide col-sm-show">Comparar</span>
+        </Link>
+      </div>
+    )
+  }
+}
 
 
 export default OrderItem
