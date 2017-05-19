9ab92069a6437e4f39b08fb8294e44dc3b6a096c
DavidMBP <steadypathapp@gmail.com>
2017-05-19 10:51:14 -0500

    Added validation messages

---
+++ backend/helpers/validationMessages.js
@@ -0,0 +1,18 @@
+var phone = require('phone');
+
+const valmsg = {
+  required: 'Necesario',
+  email: 'El correo electrónico tiene que ser válida.',
+  min: (min) => `Debe tener como mínimo ${min} caracteres`,
+  max: (max) => `Debe tener como maximo ${max} caracteres`,
+  email_unique: 'El correo electrónico ya esta registrado',
+  phone(value): () => {
+    if(phone(value, 'MX').length == 0){
+      throw new Error('Numero invalido')
+    }
+  },
+  plu_unique: 'El plu ya esta registrado'
+
+}
+
+export default valmsg
