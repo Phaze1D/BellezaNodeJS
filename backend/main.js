351165f9526d28c6e7e139761ca9a3cee8e1e19c
DavidMBP <steadypathapp@gmail.com>
2017-05-22 22:07:16 -0500

    Added loop for controllers

--- backend/main.js
+++ backend/main.js
@@ -1,18 +1,32 @@
-var express = require('express')
-var path = require("path");
-var controllers = require('./controllers/categories.js')
+'use strict'
+let express = require('express')
+let path = require("path")
+let bodyParser = require('body-parser')
+let controllers = require('./controllers')
+let errorHandlers = require('./helpers/errorHandlers.js')
 
 
-var app = express()
+
+let app = express()
 
 
 app.use(express.static('build'))
-app.use('/', controllers)
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+for (var key in controllers) {
+  if (controllers.hasOwnProperty(key)) {
+    app.use('/', controllers[key])
+  }
+}
+
+app.use(errorHandlers)
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname+'/../index.html'))
 })
 
-app.listen(3000, function () {
+app.listen(8080, function () {
   console.log('Example app listening on port 3000!')
 })
