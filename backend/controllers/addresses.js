56eaab1a13fb4ac32456b55535ce7e19972a1aed
DavidMBP <steadypathapp@gmail.com>
2017-05-22 22:03:05 -0500

    Added business logic to routes

--- backend/controllers/addresses.js
+++ backend/controllers/addresses.js
@@ -1,24 +1,60 @@
-var express = require('express')
-var router = express.Router()
+'use strict'
+let express = require('express')
+let multer = require('multer')
+let Address = require('../models').Address
 
+let upload = multer()
+let router = express.Router()
 
+let addressFields = [
+  'first_name',
+  'last_name',
+  'telephone',
+  'street',
+  'street2',
+  'city',
+  'state',
+  'zipcode',
+  'country',
+  'user_id'
+]
 
 /** REQUIRES USER VALIDATION */
-router.post('/address', function (req, res) {
-
+router.post('/address', upload.none(), function (req, res, next) {
+  Address.create(req.body, {fields: addressFields}).then(address => {
+    res.json(address)
+  }).catch(next)
 })
 
 /** REQUIRES USER VALIDATION */
-router.put('/address/:id', function (req, res) {
-
+router.put('/address/:id', upload.none(), function (req, res, next) {
+  Address.findById(req.params.id).then( address => {
+    return address.update(req.body, {fields: addressFields})
+  }).then(address => {
+    res.json(address)
+  }).catch(next)
 })
 
 /** REQUIRES USER VALIDATION */
 router.get('/validate-address', function (req, res) {
-
+  let address = Address.build(req.query, {fields: [Object.keys(req.query)[0]]})
+  address.validate({fields: [Object.keys(req.query)[0]]}).then((err) => {
+    if(err){
+      next(err)
+    }else{
+      res.sendStatus(200)
+    }
+  }).catch(next)
 })
 
 /** REQUIRES USER VALIDATION */
-router.delete('/address/:id', function (req, res) {
-
+router.delete('/address/:id', function (req, res, next) {
+  Address.findById(req.params.id).then( address => {
+    return address.destroy({ force: true })
+  }).then( () => {
+    res.sendStatus(200)
+  }).catch(next)
 })
+
+
+module.exports = router
