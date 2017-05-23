d78808d6f2c4e72c863e42b50da4929b82efffc2
DavidMBP <steadypathapp@gmail.com>
2017-05-22 22:04:18 -0500

    Added index file for controllers

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -1,1 +1,20 @@
-var controllers = {}
+'use strict'
+let categories = require('./categories.js')
+let products = require('./products.js')
+let users = require('./users.js')
+let addresses = require('./addresses.js')
+let backoffice = require('./backoffice.js')
+let discount_codes = require('./discount_codes.js')
+
+
+let controllers = {
+  categories: categories,
+  products: products,
+  users: users,
+  addresses: addresses,
+  backoffice: backoffice,
+  discount_codes: discount_codes
+}
+
+
+module.exports = controllers
