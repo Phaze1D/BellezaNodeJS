0f689d0a80446078b2c30bda42ea219711d7e3b6
DavidMBP <steadypathapp@gmail.com>
2017-05-26 23:08:07 -0500

    Finished cardPaymentFlow

--- backend/controllers/payments.js
+++ backend/controllers/payments.js
@@ -2,21 +2,34 @@
 let express = require('express')
 let isLogin = require('../middleware/user.js').isLogin
 let conektaHelper = require('../helpers/conekta.js')
+let models = require('../models')
+let Order = models.Order
 let router = express.Router()
 
 
-
-/** REQUIRES USER VALIDATION */
+// NEED TO DECREMENT STOCK
 router.post('/payment/card', isLogin, function (req, res, next) {
-  conektaHelper.cardPaymentFlow(req, (order) => {
+  conektaHelper.cardPaymentFlow(req.body, req.jwtUser.userId).then(conektaOrder => {
+    let order = req.body
+    conektaOrder = conektaOrder.toObject()
+    order.conekta_id = conektaOrder.id
+    order.status = conektaOrder.payment_status
+    order.user_id = req.jwtUser.userId
+    delete order.shipping_address_id
+    delete order.invoice_address_id
+    if(Object.keys(order.invoiceAddress).length === 0){
+      delete order.invoiceAddress
+    }
+    return Order.create(order, Order.createOptions())
+  }).then(order => {
     res.sendStatus(200)
-  },(err) => {
-    res.sendStatus(404)
+  }).catch(err => {
+    console.log(err);
+    res.sendStatus(401)
   })
 
 })
 
-/** REQUIRES USER VALIDATION */
 router.post('/payment/cash', isLogin, function (req, res, next) {
 
 })
--- backend/helpers/conekta.js
+++ backend/helpers/conekta.js
@@ -1,62 +1,158 @@
 'use strict'
-let User = require('../models').User
-let conekta = require('conekta')
+let models = require('../models')
+let conekta = require('conekta-promise')
+let User = models.User
+let Product = models.Product
+let DiscountCode = models.DiscountCode
+
 conekta.locale = 'es'
-conekta.api_key = 'key_ZnvPsaqkbDWw21riivzRKw'
+conekta.api_key = 'key_iA6gW6UnTvwRkT4CwTm7YA'
+conekta.api_version = '2.0.0'
 
-let userAtt = ['first_name', 'last_name', 'email', 'telephone', 'conekta_id']
+let userAtt = [
+  'first_name',
+  'last_name',
+  'email',
+  'telephone',
+  'conekta_id',
+  'id'
+]
+let prodAtt = [
+  'id',
+  'price',
+  'discount',
+  'iva',
+  'stock'
+]
 
-const cardPaymentFlow = (req, successCB, errorCB) => {
-  User.findOne({where: {id: req.jwtUser.userId}, attributes: userAtt})
+const VERIFICATION_ERROR = {error: 401, message: 'verification error'}
+
+const cardPaymentFlow = (cart, userId) => {
+  return verifyCart(cart, userId)
+  .then(products => {
+    return User.findOne({where: {id: userId}, attributes: userAtt})
+  })
   .then(user => {
     if(user.conekta_id){
-      createOrder(req.body, user.conekta_id, successCB, errorCB)
+      return user
     }else{
-      conekta.Customer.create({
+      return conekta.Customer.create({
         name: `${user.first_name} ${user.last_name}`,
         email: user.email,
         phone: user.telephone,
-      }, (err, customer) => {
-        if(err) errorCB(err);
-
-        let order = formatOrder(req.body, customer.toObject().id)
-        conekta.Order.create(order, (err, res) => {
-          console.log(err);
-          if(res) console.log(res.toObject());
-          successCB()
-        })
+      }).then(conCust => {
+        return user.update({conekta_id: conCust.toObject().id})
       })
     }
-  }).catch(errorCB)
+  })
+  .then(user => {
+    let formatted = formatOrder(cart, user.conekta_id)
+    formatted.charges = [{
+      payment_method: {
+        type: 'card',
+        token_id: 'tok_test_visa_4242',
+      },
+      amount: cart.total
+    }]
+    return conekta.Order.create(formatted)
+  })
 }
 
-const verifyCart = (cart) => {
+const verifyCart = (cart, userId) => {
+  let pin = cart.details.map(detail => detail.product_id)
+  let oDetails = {}
+  cart.details.forEach(detail => oDetails[detail.product_id] = detail)
 
+  return DiscountCode.findById(cart.discount_code_id).then(discount_code => {
+    discount_code = discount_code ? discount_code.toJSON() : null
+    return Product.findAll({where: {id: {$in: pin }}, attributes: prodAtt})
+    .then(products => {
+      let sub_total = 0
+      let iva_total = 0
+      let shipping_total = 0
+      products.forEach(product => {
+        product = product.toJSON()
+
+        if( !(oDetails[product.id] && oDetails[product.id].quantity <= product.stock) ){
+          throw VERIFICATION_ERROR.info = {car: oDetails[product.id].quantity, ser: product.stock}
+        }
+
+        let pPrice = Math.round(product.price * (1 - product.discount/100))
+        let dPrice = Math.round(oDetails[product.id].price * (1 - oDetails[product.id].discount/100))
+
+        if( dPrice != pPrice){
+          throw VERIFICATION_ERROR.info = {dPrice: dPrice, pPrice: pPrice}
+        }
+
+        sub_total += pPrice * oDetails[product.id].quantity
+        iva_total += Math.round(pPrice * oDetails[product.id].quantity * (product.iva/100))
+      })
+
+      shipping_total = sub_total < 1000 ? 150 : 0
+
+
+      if( !(sub_total == cart.sub_total && iva_total == cart.iva_total && shipping_total == cart.shipping_total) ){
+        throw VERIFICATION_ERROR.info = {
+          subt: sub_total,
+          csubt: cart.sub_total,
+          ivat: iva_total,
+          civat: cart.iva_total,
+          ship: shipping_total,
+          cship: cart.shipping_total
+        }
+      }
+
+      let total = sub_total + iva_total + shipping_total
+
+      if(cart.discount_code_id){
+        if(cart.discount_code_id == discount_code.id && discount_code.user_id == userId){
+          let discount_total = discount_code.is_percentage ? Math.round(total * (discount_code.discount/100)) : discount_code.discount
+          total -= discount_total
+        }else{
+          throw VERIFICATION_ERROR.info = {
+            cdis: cart.discount_code_id,
+            dis: discount_code.id,
+            disU: discount_code.user_id,
+            u: userId
+          }
+        }
+      }
+
+      if(total != cart.total){
+        throw VERIFICATION_ERROR.info = {
+          tot: total,
+          ctot: cart.totaln
+        }
+      }
+
+      return products
+    })
+  })
 }
 
 const formatOrder = (cart, customer_id) => {
   let line_items = cart.details.map(detail => {
     return {
       name: detail.name,
-      unit_price: Math.round( (detail.price * (1 - (detail.discount/100) ) ) * 100),
+      unit_price: Math.round( detail.price * (1 - (detail.discount/100)) ),
       quantity: detail.quantity,
     }
   })
 
   let shipping_line = {
-    amount: Math.round(cart.shipping_total * 100),
+    amount: cart.shipping_total,
     carrier: 'RedPax'
   }
 
   let tax_line = {
     description: 'IVA',
-    amount: Math.round(cart.iva_total * 100)
+    amount: cart.iva_total
   }
 
   let discount_line = null
   if(cart.discount_code_id){
     discount_line = {
-      amount: Math.round(cart.discount_total * -100),
+      amount: cart.discount_total,
       code: `${cart.discount_code_id*100}`,
       type: 'coupon'
     }
@@ -75,18 +171,6 @@ const formatOrder = (cart, customer_id) => {
     }
   }
 
-  let expires_at = new Date
-  expires_at.setDate(expires_at.getDate() + 3)
-
-  let charge = {
-    payment_method: {
-      type: 'card',
-      token_id: cart.payment_source.token,
-    },
-    livemode: false,
-    amount: Math.round(cart.total * 100)
-  }
-
   return {
     customer_info: {customer_id: customer_id},
     currency: "MXN",
@@ -95,7 +179,6 @@ const formatOrder = (cart, customer_id) => {
     tax_lines: [tax_line],
     discount_lines: [discount_line],
     shipping_contact: shipping_contact,
-    charges: [charge]
   }
 }
 
