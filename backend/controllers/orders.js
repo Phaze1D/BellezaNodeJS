f63579653c82a97acf689c199c54c5df18f40f91
DavidMBP <steadypathapp@gmail.com>
2017-05-26 23:07:51 -0500

    Removed comments

--- backend/controllers/orders.js
+++ backend/controllers/orders.js
@@ -8,14 +8,12 @@ let isAdmin = middware.isAdmin
 let router = express.Router()
 
 
-/** REQUIRES ADMIN VALIDATION */
 router.get('/orders', isLogin, isAdmin, function (req, res, next) {
   Order.findAndCountAll(Order.allOptions(req.query)).then(results => {
     res.json(results)
   }).catch(next)
 })
 
-/** REQUIRES USER OR ADMIN VALIDATION */
 router.get('/user/:user_id/orders', isLogin, isAdmin, function (req, res, next) {
   Order.findAndCountAll(Order.userAllOptions(req.query, req.params.user_id))
   .then(results => {
@@ -23,9 +21,8 @@ router.get('/user/:user_id/orders', isLogin, isAdmin, function (req, res, next) 
   }).catch(next)
 })
 
-/** REQUIRES USER OR ADMIN VALIDATION */
 router.get('/user/:user_id/order/:id', isLogin, isAdmin, function (req, res, next) {
-  Order.findOne({where: {id: req.params.id, user_id: req.params.user_id}})
+  Order.findOne(Order.singleOption(req.params))
   .then(order => {
     if(order){
       res.json(order)
