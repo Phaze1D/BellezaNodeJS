00aed5a6cc8e7d68ebad07d4de9773423652bf9e
DavidMBP <steadypathapp@gmail.com>
2017-06-01 18:01:26 -0500

    Added correct payment info
    Created oxxo and spei payment info to display when the
    user pays with cash

--- frontend/pages/checkout/successful.jsx
+++ frontend/pages/checkout/successful.jsx
@@ -1,4 +1,5 @@
 import React, { PropTypes } from 'react'
+import { Link, Redirect } from 'react-router-dom'
 import { Address } from './confirmation'
 import OrderTable from 'components/OrderTable/OrderTable'
 import { connect } from 'react-redux'
@@ -19,7 +20,6 @@ import { resetCart } from 'actions/cart'
   return {
     cart: store.cart,
     user: store.user,
-    payment: store.payment
   }
 })
 class CheckoutSuccessful extends React.Component {
@@ -32,7 +32,6 @@ class CheckoutSuccessful extends React.Component {
     const {
       cart,
       user,
-      payment,
     } = this.props
 
     if(!user.get('token')){
@@ -41,20 +40,32 @@ class CheckoutSuccessful extends React.Component {
 
     return (
       <main>
-        <h2 className="col-8 col-xs-12" style={{color: '#1eab30'}}>Procesado su Pedido</h2>
-        <p className="col-8 col-xs-12">
-          Tu pedido está siendo procesado
-          <br/>
-					Te hemos enviado instrucciones a su correo electrónico
-          (<span className="sub-text light"> {user.get('email')} </span>) acerca de su estado de la orden
-          <br/>
-				  Cuando hayamos confirmado su pago, usted recibirá información sobre su envío.
-        </p>
+        {cart.getIn(['charges', 'payment_method', 'type']) === 'oxxo' &&
+          <OxxoSuccess
+            email={user.get('email')}
+            total={cart.get('total')}
+            reference={cart.getIn(['charges', 'payment_method', 'reference'])}/>
+        }
+
+        {cart.getIn(['charges', 'payment_method', 'type']) === 'credit' &&
+          <CardSuccess email={user.get('email')}/>
+        }
+
+        { cart.getIn(['charges', 'payment_method', 'type']) === 'spei' &&
+          <BankSuccess
+            email={user.get('email')}
+            total={cart.get('total')}
+            clabe={cart.getIn(['charges', 'payment_method', 'clabe'])}/>
+        }
 
         <div className="col-8 col-xs-12">
           <Address address={cart.get('shippingAddress')} title="Dirección de Envío"/>
           {!cart.get('invoiceAddress').isEmpty() &&
-            <Address address={cart.get('invoiceAddress')} title="Facturacion" rfc={cart.get('rfc')} razonSocial={cart.get('razon_social')}/>
+            <Address
+              address={cart.get('invoiceAddress')}
+              title="Facturacion"
+              rfc={cart.get('rfc')}
+              razonSocial={cart.get('razon_social')}/>
           }
           <OrderTable
             order={cart}
@@ -66,3 +77,101 @@ class CheckoutSuccessful extends React.Component {
 }
 
 export default CheckoutSuccessful;
+
+
+const CardSuccess = (props) => (
+  <div className="col-8 col-xs-12">
+    <h2 style={{color: '#1eab30'}}>Pago Exitoso</h2>
+    <p>
+      Tu pedido está siendo procesado
+      <br/>
+      Te hemos enviado instrucciones a su correo electrónico
+      (<span className="sub-text light"> {props.email} </span>) acerca de su estado de la orden
+      <br/>
+    </p>
+  </div>
+)
+
+
+const OxxoSuccess = (props) => (
+  <div className="col-8 col-xs-12">
+    <h2 style={{color: '#FFC107'}}>En Espera de Pago</h2>
+    <p>
+      Cuando el pago se ha hecho, enviaremos su pedido
+      <br/>
+      Te hemos enviado instrucciones a su correo electrónico
+      (<span className="sub-text light">{props.email}</span>) acerca de su estado de la orden
+      <br/>
+    </p>
+
+    <div className="oxxo-sub">
+      <div className="opps-info">
+        <div className="opps-brand"><img src="https://raw.githubusercontent.com/conekta-examples/oxxopay-payment-stub/master/demo/oxxopay_brand.png" alt="OXXOPay"/></div>
+        <div className="opps-amount">
+          <h3>Monto a pagar</h3>
+          <h2>${(props.total/100).toFixed(2)}<sup>MXN</sup></h2>
+          <p className="sub-text">OXXO cobrará una comisión adicional al momento de realizar el pago.</p>
+          <p className="sub-text">Esto expira en 3 días</p>
+        </div>
+      </div>
+
+      <div className="box ref-box">
+        <h3>Referencia</h3>
+        <h1>{props.reference}</h1>
+      </div>
+
+      <h3>Instrucciones</h3>
+
+      <ol>
+        <li>Acude a la tienda OXXO más cercana. <a href="https://www.google.com.mx/maps/search/oxxo/" target="_blank">Encuéntrala aquí</a>.</li>
+        <li>Indica en caja que quieres ralizar un pago de <strong>OXXOPay</strong>.</li>
+        <li>Dicta al cajero el número de referencia en esta ficha para que tecleé directamete en la pantalla de venta.</li>
+        <li>Realiza el pago correspondiente con dinero en efectivo.</li>
+        <li>Al confirmar tu pago, el cajero te entregará un comprobante impreso.
+          En el podrás verificar que se haya realizado correctamente.
+          Conserva este comprobante de pago.
+        </li>
+      </ol>
+    </div>
+  </div>
+)
+
+const BankSuccess = (props) => (
+  <div className="col-8 col-xs-12">
+    <h2 style={{color: '#FFC107'}}>En Espera de Pago</h2>
+    <p>
+      Cuando el pago se ha hecho, enviaremos su pedido
+      <br/>
+      Te hemos enviado instrucciones a su correo electrónico
+      (<span className="sub-text light">{props.email}</span>) acerca de su estado de la orden
+      <br/>
+    </p>
+
+    <div className="oxxo-sub">
+      <div className="opps-info">
+        <div className="opps-brand"><img src="https://raw.githubusercontent.com/conekta-examples/spei-payment-stub/master/demo/spei_brand.png" alt="SPEI"/></div>
+        <div className="opps-amount">
+          <h3>Monto a pagar</h3>
+          <h2>${(props.total/100).toFixed(2)}<sup>MXN</sup></h2>
+          <p className="sub-text">Esto expira en 3 días</p>
+        </div>
+      </div>
+
+      <div className="box ref-box">
+        <h3>Clabe</h3>
+        <h1>{props.clabe}</h1>
+      </div>
+
+      <h3>Instrucciones</h3>
+
+      <ol>
+        <ol>
+					<li>Accede a tu banca en línea.</li>
+					<li>Da de alta la CLABE en esta ficha. <strong>El banco deberá de ser STP</strong>.</li>
+					<li>Realiza la transferencia correspondiente por la cantidad exacta en esta ficha, <strong>de lo contrario se rechazará el cargo</strong>.</li>
+					<li>Al confirmar tu pago, el portal de tu banco generará un comprobante digital. <strong>En el podrás verificar que se haya realizado correctamente.</strong> Conserva este comprobante de pago.</li>
+				</ol>
+      </ol>
+    </div>
+  </div>
+)
