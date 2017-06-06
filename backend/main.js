28e57335ddda0c0cabc5afc2a114dfbb4b926c2e
DavidMBP <steadypathapp@gmail.com>
2017-06-06 13:41:51 -0500

    Added fake s3 keys

--- backend/main.js
+++ backend/main.js
@@ -8,6 +8,8 @@ let app = express()
 
 app.set('SECRET', 'shhhh')
 app.set('CONEKTA', 'key_oF4vKY43ZW7romYgdv5wNw')
+app.set('S3_SECRET_KEY', '5HNRr765wUlO4feBTuHVuSbRASMIalIMh+eHi+U4')
+app.set('S3_ID', 'AKIAIM3QDCFQJONTWL7Q')
 
 app.use(express.static('build'))
 app.use(bodyParser.json())
