ed817144d7e7c2ac9d3dce8e7d7577653cd3f5a8
DavidMBP <steadypathapp@gmail.com>
2017-06-06 22:05:05 -0500

    Created aws helper and image validation

---
+++ backend/helpers/aws.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  uploadBanner: function (body, name) {
+    return {
+      ACL: 'public-read',
+      Body: body,
+      Bucket: 'belleza-node',
+      CacheControl: 'public, max-age=3153600',
+      ContentType: 'image/jpeg',
+      Key: `banners/${name}`,
+    }
+  }
+}
--- backend/helpers/validationMessages.js
+++ backend/helpers/validationMessages.js
@@ -1,5 +1,9 @@
 'use strict'
 let phone = require('phone');
+let Sequelize = require('sequelize')
+let Promise = require('bluebird');
+let gm = require('gm')
+Promise.promisifyAll(gm.prototype);
 
 const valmsg = {
   required: 'Necesario',
@@ -14,7 +18,24 @@ const valmsg = {
       throw new Error('Numero invalido')
     }
   },
+  bannerImage: (lg, sm) => {
+    return Promise.all([gm(lg).sizeAsync(), gm(sm).sizeAsync()]).then( values => {
+      if( !(values[0].width == 940 && values[0].height == 300) ){
+        let err = new Sequelize.ValidationError('')
+        err.errors.push({path: 'imagelg', message: 'Image must be 940 by 300'})
+        return Promise.reject(err)
+      }
 
+      if( !(values[1].width == 540 && values[1].height == 300) ){
+        let err = new Sequelize.ValidationError('')
+        err.errors.push({path: 'imagesm', message: 'Image must be 540 by 300'})
+        return Promise.reject(err)
+      }
+
+      return values
+    })
+
+  }
 }
 
 module.exports = valmsg;
