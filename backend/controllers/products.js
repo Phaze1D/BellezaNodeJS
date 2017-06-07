0054b665d61cd2ab89adfe669c733f137fe3607f
DavidMBP <steadypathapp@gmail.com>
2017-06-07 16:55:09 -0500

    Created the s3 product image upload

--- backend/controllers/products.js
+++ backend/controllers/products.js
@@ -5,6 +5,9 @@ let Sequelize = require('sequelize')
 let multer = require('multer')
 let models = require('../models')
 let middleware = require('../middleware/user.js')
+let productImage = require('../helpers/validationMessages.js').productImage
+let aws = require('aws-sdk');
+let awsHelper = require('../helpers/aws.js')
 
 let isLogin = middleware.isLogin
 let isAdmin = middleware.isAdmin
@@ -27,6 +30,11 @@ let productFields = [
   'stock',
   'active',
   'fav'
+]
+
+let productFiles = [
+  {name: 'main_image'},
+  {name: 'second_image'},
 ]
 
 
@@ -60,19 +68,77 @@ router.get('/backoffice/products', isLogin, isAdmin, function (req, res, next) {
   }).catch(next)
 })
 
-router.post('/product', isLogin, isAdmin, upload.none(), function (req, res, next) {
-  Product.create(req.body, {fields: productFields}).then(product => {
-    res.json(product)
+router.post('/product', isLogin, isAdmin, upload.fields(productFiles), function (req, res, next) {
+  let mainimg = req.files.main_image ? req.files.main_image[0].buffer : null
+  let secimg = req.files.second_image ? req.files.second_image[0].buffer : null
+
+  productImage(mainimg, secimg).then(buffers => {
+    return Product.create(req.body, {fields: productFields}).then(product => {
+      let s3 = new aws.S3({
+        accessKeyId: req.app.get('S3_ID'),
+        secretAccessKey: req.app.get('S3_SECRET_KEY'),
+      })
+      let uploads = [
+        s3.upload(awsHelper.uploadS3(buffers[0], `products/lg/${product.plu}.jpg`)).promise(),
+        s3.upload(awsHelper.uploadS3(buffers[1], `products/md/${product.plu}.jpg`)).promise(),
+        s3.upload(awsHelper.uploadS3(buffers[2], `products/sm/${product.plu}.jpg`)).promise(),
+        s3.upload(awsHelper.uploadS3(buffers[3], `products/xs/${product.plu}.jpg`)).promise(),
+        s3.upload(awsHelper.uploadS3(buffers[4], `products/xxs/${product.plu}.jpg`)).promise(),
+
+        s3.upload(awsHelper.uploadS3(buffers[5], `products/lg/${product.plu}_2.jpg`)).promise(),
+        s3.upload(awsHelper.uploadS3(buffers[6], `products/md/${product.plu}_2.jpg`)).promise(),
+        s3.upload(awsHelper.uploadS3(buffers[7], `products/sm/${product.plu}_2.jpg`)).promise(),
+        s3.upload(awsHelper.uploadS3(buffers[8], `products/xs/${product.plu}_2.jpg`)).promise(),
+        s3.upload(awsHelper.uploadS3(buffers[9], `products/xxs/${product.plu}_2.jpg`)).promise(),
+      ]
+
+      return Promise.all(uploads)
+    })
+  }).then(data => {
+    res.sendStatus(200)
   }).catch(next)
 })
 
-router.put('/product/:id', isLogin, isAdmin, upload.none(), function (req, res, next) {
+router.put('/product/:id', isLogin, isAdmin, upload.fields(productFiles), function (req, res, next) {
+  let mainimg = req.files.main_image ? req.files.main_image[0].buffer : null
+  let secimg = req.files.second_image ? req.files.second_image[0].buffer : null
+  let realProduct = null
   Product.findById(req.params.id).then(product => {
+    realProduct = product
     return product.update(req.body, {fields: productFields.slice(1)})
   }).then(product => {
-    return product.setCategories(req.body.categories)
-  }).then(product => {
-    res.json(product)
+    if(req.body.categories){
+      return product.setCategories(req.body.categories)
+    }
+    return product
+  }).then(procates => {
+    if(mainimg && secimg){
+      return productImage(mainimg, secimg).then(buffers => {
+        let s3 = new aws.S3({
+          accessKeyId: req.app.get('S3_ID'),
+          secretAccessKey: req.app.get('S3_SECRET_KEY'),
+        })
+        let uploads = [
+          s3.upload(awsHelper.uploadS3(buffers[0], `products/lg/${realProduct.plu}.jpg`)).promise(),
+          s3.upload(awsHelper.uploadS3(buffers[1], `products/md/${realProduct.plu}.jpg`)).promise(),
+          s3.upload(awsHelper.uploadS3(buffers[2], `products/sm/${realProduct.plu}.jpg`)).promise(),
+          s3.upload(awsHelper.uploadS3(buffers[3], `products/xs/${realProduct.plu}.jpg`)).promise(),
+          s3.upload(awsHelper.uploadS3(buffers[4], `products/xxs/${realProduct.plu}.jpg`)).promise(),
+
+          s3.upload(awsHelper.uploadS3(buffers[5], `products/lg/${realProduct.plu}_2.jpg`)).promise(),
+          s3.upload(awsHelper.uploadS3(buffers[6], `products/md/${realProduct.plu}_2.jpg`)).promise(),
+          s3.upload(awsHelper.uploadS3(buffers[7], `products/sm/${realProduct.plu}_2.jpg`)).promise(),
+          s3.upload(awsHelper.uploadS3(buffers[8], `products/xs/${realProduct.plu}_2.jpg`)).promise(),
+          s3.upload(awsHelper.uploadS3(buffers[9], `products/xxs/${realProduct.plu}_2.jpg`)).promise(),
+        ]
+
+        return Promise.all(uploads)
+      })
+    }
+    return procates
+  }).then(data => {
+    console.log(data);
+    res.sendStatus(200)
   }).catch(next)
 })
 
