0d0070bc1ad2c105eeffad3b9201a94d4b28229b
DavidMBP <steadypathapp@gmail.com>
2017-06-08 21:44:47 -0500

    Finished the payments emails missing webhook

--- backend/controllers/payments.js
+++ backend/controllers/payments.js
@@ -2,8 +2,10 @@
 let express = require('express')
 let isLogin = require('../middleware/user.js').isLogin
 let conektaHelper = require('../helpers/conekta.js')
+let emailSender = require('../helpers/emailSender.js')
 let models = require('../models')
 let Order = models.Order
+let User = models.User
 let router = express.Router()
 
 const paymentStatus = {
@@ -31,13 +33,33 @@ router.post('/payment/card', isLogin, function (req, res, next) {
 
     return Order.create(order, Order.createOptions()).then(order => {
       let jorder = order.toJSON()
-      jorder.charges = conektaOrder.charges.data[0]
-      res.json(jorder)
+      jorder.charges = {
+        payment_method: {
+          type: conektaOrder.charges.data[0].payment_method.type,
+          reference: conektaOrder.charges.data[0].payment_method.reference,
+          clabe: conektaOrder.charges.data[0].payment_method.clabe,
+        }
+      }
+
+      jorder.details = req.body.details
+
+      User.findOne({where: {id: req.jwtUser.userId}, attributes: ['first_name', 'last_name', 'email']})
+      .then( user => {
+        req.app.render('paid_info', {order: jorder, user: user.toJSON()}, function (err, html) {
+          if(err) next(err);
+          res.json(jorder)
+
+          let sesConfig = {
+            accessKeyId: req.app.get('SES_ID'),
+            secretAccessKey: req.app.get('SES_SECRET_KEY'),
+            region: req.app.get('SES_REGION')
+          }
+          emailSender.sendEmail(user.toJSON().email, html, sesConfig)
+        })
+      })
+
     })
-  }).catch(err => {
-    console.log(err);
-    res.sendStatus(500)
-  })
+  }).catch(next)
 })
 
 router.post('/payment/cash', isLogin, function (req, res, next) {
@@ -59,13 +81,32 @@ router.post('/payment/cash', isLogin, function (req, res, next) {
 
     return Order.create(order, Order.createOptions()).then(order => {
       let jorder = order.toJSON()
-      jorder.charges = conektaOrder.charges.data[0]
-      res.json(jorder)
+      jorder.charges = {
+        payment_method: {
+          type: conektaOrder.charges.data[0].payment_method.type,
+          reference: conektaOrder.charges.data[0].payment_method.reference,
+          clabe: conektaOrder.charges.data[0].payment_method.clabe,
+        }
+      }
+      jorder.details = req.body.details
+
+      User.findOne({where: {id: req.jwtUser.userId}, attributes: ['first_name', 'last_name', 'email']})
+      .then( user => {
+        let type = jorder.charges.payment_method.type
+        req.app.render(`${type}_info`, {order: jorder, user: user.toJSON()}, function (err, html) {
+          if(err) next(err);
+          res.json(jorder)
+
+          let sesConfig = {
+            accessKeyId: req.app.get('SES_ID'),
+            secretAccessKey: req.app.get('SES_SECRET_KEY'),
+            region: req.app.get('SES_REGION')
+          }
+          emailSender.sendEmail(user.toJSON().email, html, sesConfig)
+        })
+      })
     })
-  }).catch(err => {
-    console.log(err);
-    res.sendStatus(500)
-  })
+  }).catch(next)
 })
 
 module.exports = router
