aea9085c8fc269dd242e395bed625ba873b52f2b
DavidMBP <steadypathapp@gmail.com>
2017-06-11 18:54:02 -0500

    Finished global error handling

--- frontend/components/GlobalError/GlobalError.jsx
+++ frontend/components/GlobalError/GlobalError.jsx
@@ -1,22 +1,43 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import classnames from 'classnames'
+import { resetErrors } from 'actions/errors'
+
 
 
 @connect( store => {
   return {
-    global: store.errors.global
+    global: store.errors.get('global')
   }
 })
 export default class GlobalError extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {show: false}
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.global.get('status')){
+      this.setState({show: true})
+
+      setTimeout(() => {
+        this.setState({show: false})
+        this.props.dispatch(resetErrors())
+      },  2000)
+    }
   }
 
   render(){
+    const {
+      global
+    } = this.props
+
+    const errClasses = classnames({'show': this.state.show})
 
     return (
-      <div id="error-box">
-        
+      <div id="error-box" className={errClasses}>
+        <h3>Error {global.get('status')}</h3>
+        <p>{global.get('message')}</p>
 
       </div>
     )
