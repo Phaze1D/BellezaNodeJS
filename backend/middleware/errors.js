313dc9e8cc7040d015cdde72ccc374c6bdad3bac
DavidMBP <steadypathapp@gmail.com>
2017-06-12 12:04:39 -0500

    Fixed bug
    Error: Can't set headers after they are sent.

--- backend/middleware/errors.js
+++ backend/middleware/errors.js
@@ -8,21 +8,15 @@ const errorMiddleware = (err, req, res, next) => {
     let cerr = new Sequelize.ValidationError('')
     cerr.errors.push({path: 'card_token', message: err.details[0].message})
     res.status(422).json(cerr.errors)
-  }
-
-  if(err.name === "SequelizeValidationError"){
+  }else if(err.name === "SequelizeValidationError"){
     res.status(422).json(err.errors)
-  }
-
-  if(err.name === "SequelizeEmptyResultError"){
+  }else if(err.name === "SequelizeEmptyResultError"){
     res.status(404).json(err.errors)
-  }
-
-  if(err.name === "VerificationError"){
+  }else if(err.name === "VerificationError"){
     res.status(401).json(err)
+  }else {
+    next(err)
   }
-
-  next(err)
 }
 
 
