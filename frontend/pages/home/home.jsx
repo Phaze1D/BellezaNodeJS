3b7f1bed0b3b1f42a6a33d032367e26afdc6dc7e
DavidMBP <steadypathapp@gmail.com>
2017-06-13 22:22:22 -0500

    Fixed bug for server side rendering

--- frontend/pages/home/home.jsx
+++ frontend/pages/home/home.jsx
@@ -59,13 +59,19 @@ export default class Home extends React.Component {
 
     return (
       <main>
-        <Loader>
+        {banners.size > 0 && products.size > 0 ?
           <div>
             <Carousel banners={banners}/>
             <Favorites products={products}/>
           </div>
-        </Loader>
-
+          :
+          <Loader>
+            <div>
+              <Carousel banners={banners}/>
+              <Favorites products={products}/>
+            </div>
+          </Loader>
+        }
 
         <div className="grid col-sm-hide">
           <div className="col-4 social-feed">
@@ -97,28 +103,33 @@ export default class Home extends React.Component {
 
 
 const facebookScript = function () {
-  (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.9";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
+  if(typeof window !== 'undefined'){
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.9";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  }
 }
 
 const twitterScript = function () {
-  window.twttr = (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0],
-      t = window.twttr || {};
-    if (d.getElementById(id)) return t;
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "https://platform.twitter.com/widgets.js";
-    fjs.parentNode.insertBefore(js, fjs);
-    t._e = [];
-    t.ready = function(f) {
-      t._e.push(f);
-    };
-    return t;
-  }(document, "script", "twitter-wjs"));
+
+  if(typeof window !== 'undefined'){
+    window.twttr = (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0],
+        t = window.twttr || {};
+      if (d.getElementById(id)) return t;
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "https://platform.twitter.com/widgets.js";
+      fjs.parentNode.insertBefore(js, fjs);
+      t._e = [];
+      t.ready = function(f) {
+        t._e.push(f);
+      };
+      return t;
+    }(document, "script", "twitter-wjs"));
+  }
 }
