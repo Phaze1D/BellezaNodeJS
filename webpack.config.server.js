c1883247f2d5baf37aa6a7d22210f3ca3a0bb54e
DavidMBP <steadypathapp@gmail.com>
2017-06-16 00:35:47 -0500

    Created the webpack server side config

---
+++ webpack.config.server.js
@@ -0,0 +1,45 @@
+var webpack = require('webpack');
+var path = require('path');
+var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+var nodeExternals = require('webpack-node-externals');
+
+
+var config = {
+  target: 'node',
+  externals: [nodeExternals()],
+  node: {
+    __dirname: true,
+    __filename: true,
+  },
+  entry: './backend/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'backend/build'),
+    publicPath: '/'
+  },
+  resolve: {
+    modules: [
+      'node_modules',
+      'frontend'
+    ],
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      exclude: /(node_modules)/,
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: [['es2015', {modules: false}], 'react'],
+          plugins: ["transform-decorators-legacy", "transform-ensure-ignore"]
+        }
+      }]
+    }]
+  },
+  plugins: [
+    new UglifyJSPlugin(),
+  ],
+}
+
+module.exports = config
