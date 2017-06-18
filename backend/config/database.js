a5f627765eb6901556374f6b51f45ee7cad1977a
DavidMBP <steadypathapp@gmail.com>
2017-06-17 19:25:27 -0500

    Added default options for production

--- backend/config/database.js
+++ backend/config/database.js
@@ -11,10 +11,10 @@ module.exports = {
     }
   },
   "production": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
+    "username": process.env.DB_USERNAME || "root",
+    "password": process.env.DB_PASSWORD || null,
+    "database": process.env.DB_NAME || "bellezaNodeJS",
+    "host": process.env.DB_HOST || "127.0.0.1",
     "dialect": "mysql",
     "define": {
       "timestamps": true,
