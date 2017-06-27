dadc059ff89a0c299b7ebd98ff9b04faa3792f7a
DavidMBP <steadypathapp@gmail.com>
2017-06-26 20:10:05 -0500

    Fixed syntax error

--- frontend/components/ProductForm/ProductForm.jsx
+++ frontend/components/ProductForm/ProductForm.jsx
@@ -118,7 +118,7 @@ class ProductForm extends React.Component {
 						<label>Price</label>
 						{errors.get("price") && <div className="error-div">{errors.get("price")}</div>}
 						<input type="number" min="0" step="any" name="price" className="input input-quantity"
-							defaultValue={(product.get("price"/100)).toFixed(2)}/>
+							defaultValue={(product.get("price")/100).toFixed(2)}/>
 					</div>
 
 					<div className="col-3">
